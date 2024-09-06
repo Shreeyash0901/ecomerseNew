e7c313b89138ff0fc516310de8f4b7183db92fe0
shree <itsmeyash02@gmail.com>
2024-09-06 20:28:58 +0530

    effects added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero/Hero';
+import Product from './components/Products/Product';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 function App() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-out-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <Product />
     </div>
   );
 }
